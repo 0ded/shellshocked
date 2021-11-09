a83d08a1842721f2a463e535a99cf22f4bd6e1c8
OdedGinat2 <odedginat@gmail.com>
2021-11-09 23:53:13 +0200

    fixed weird edge cases

--- guillotine.py
+++ guillotine.py
@@ -7,11 +7,13 @@ import re
 
 
 def main():
-    # get_funcs("WinQuake/cd_null.c")
-    filename = sys.argv[1]
-    funcs = get_funcs_split(filename)
-    create_h_file(filename, funcs)
-    pass
+    for i in range(1, len(sys.argv)):
+        filename = sys.argv[i]
+        inb = create_inb(filename)
+        inb = remove_block(inb)
+        inb = remove_commands(inb)
+        inb = remove_includes(inb)
+        create_h_file(filename, inb)
 
 
 def get_funcs_split(filename):
@@ -19,20 +21,79 @@ def get_funcs_split(filename):
         file = file.readlines()
         out = ""
         for line in file:
-            out += (line.replace("\n", " "))
-        # print(out)
-        # inb = re.split('{.*?}', file)
-        inb = re.sub("//.*?$|/\*.*?\*/|{.*?}|#.*?(\".*?\"|>)", '', out, flags=re.S)
-        inb = re.split('  +', inb)
+            out += (line.replace("\n", "!!"))
+        # inb = re.sub("{.*?}|//.*?$|/\*.*?\*/|#.*?(\".*?\"|>)", '', out)
+        inb = re.sub("/\*.*?\*/|//.*?!!", '', out)
+        inb = re.sub("{.*?}", '', inb)
+        inb = inb.replace("!!", "\n")
+        inb = re.sub("#.*?(\".*?\"|>)", '', inb)
+        # inb = inb.replace("\n", "")
+        inb = re.split('  +|\n', inb)
     return inb
 
 
+# removes comments + creats inb
+def create_inb(filename):
+    with open(filename, "r") as file:
+        file = file.readlines()
+        out = ""
+        for line in file:
+            out += (line.replace("\n", "!!"))
+        inb = re.sub("/\*.*?\*/|//.*?!!", '', out)
+        inb = inb.replace("!!", "\n")
+    return inb
+
+
+def split_functions(inb):
+    pass
+
+
+def remove_brackets(inb):
+    inb = inb.replace("{", "")
+    inb = inb.replace("}", "")
+    return inb
+
+
+def remove_commands(inb):
+    inb = re.sub("\n.*?;", '', inb)
+    inb = re.sub("(if|while|else|else if).*?\n", '', inb)
+    # inb = re.sub("for.*?;.*?;.*?\)", '', inb)
+    return inb
+
+
+def remove_block(inb):
+    get_sub = False
+    count = 0
+    sub = ""
+    for char in inb:
+        if get_sub:
+            sub += char
+        if char == "{":
+            if count == 0:
+                get_sub = True
+                sub += "{"
+            count += 1
+        if char == "}":
+            count -= 1
+            if count == 0:
+                inb = inb.replace(sub, "")
+                sub = ""
+                get_sub = False
+    return inb
+
+
+def remove_includes(inb):
+    inb = re.sub("#.*?(\".*?\"|>)", '', inb)
+    return inb
+
 def create_h_file(filename, funcs):
     h_file = ".h".join(filename.rsplit(".c", 1))
+    open(h_file, 'w').close()
     with open(h_file, "a") as file:
-        for func in funcs:
-            if(func != ""):
-                file.write(func+";\n")
+        for func in funcs.split("\n"):
+            if func:
+                file.write(func +";\n")
+        file.close()
 
 
 if __name__ == "__main__":
